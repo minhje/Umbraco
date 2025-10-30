6d1eda4c0ef903817104452cd9edf0e6b190f2cb
minhje <mminnahjelm@gmail.com>
2025-10-30 11:24:02 +0100

    section design done

---
+++ UmbracoProject1/ViewModels/CallbackFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoProject1.ViewModels;
+
+public class CallbackFormViewModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    [Display(Name = "Name")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email is required")]
+    [Display(Name = "Email adress")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email adress")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Phone is required")]
+    [Display(Name = "Phone")]
+    public string Phone { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please select an option")]
+    public string SelectedOption { get; set; } = null!;
+}
