8635364cb10e7e4d33b268b4b8b27c2a89611574
minhje <mminnahjelm@gmail.com>
2025-10-31 10:05:30 +0100

    form saves data to backoffice

---
+++ UmbracoProject1/Controllers/FormController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+using UmbracoProject1.Services;
+using UmbracoProject1.ViewModels;
+namespace UmbracoProject1.Controllers;
+
+public class FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, FormSubmissionsService formSubmissions) : SurfaceController(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+{
+    private readonly FormSubmissionsService _formSubmissions = formSubmissions;
+    public IActionResult HandleCallbackForm(CallbackFormViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return CurrentUmbracoPage();
+        }
+
+        var result = _formSubmissions.SaveCallbackRequest(model);
+        if (!result)
+        {
+            // formerror
+            return RedirectToCurrentUmbracoPage();
+        }
+        
+        // formsucess
+        return RedirectToCurrentUmbracoPage();
+    }
+}
