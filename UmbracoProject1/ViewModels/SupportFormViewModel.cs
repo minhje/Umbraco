e05fa856f6a8ef999d7416c33648b26da780a99d
minhje <mminnahjelm@gmail.com>
2025-11-05 10:07:30 +0100

    form design done

---
+++ UmbracoProject1/ViewModels/SupportFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoProject1.ViewModels;
+
+public class SupportFormViewModel
+{
+    [Required(ErrorMessage = "Email is required")]
+    [Display(Name = "Email adress")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email adress")]
+    public string Email { get; set; } = null!;
+}
