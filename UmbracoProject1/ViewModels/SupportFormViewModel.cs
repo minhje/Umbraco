559d8ccb446828298393be14405eff1b8531c4fc
minhje <mminnahjelm@gmail.com>
2025-11-06 14:00:17 +0100

    detail page responsive

--- UmbracoProject1/ViewModels/SupportFormViewModel.cs
+++ UmbracoProject1/ViewModels/SupportFormViewModel.cs
@@ -5,7 +5,7 @@ namespace UmbracoProject1.ViewModels;
 public class SupportFormViewModel
 {
     [Required(ErrorMessage = "Email is required")]
-    [Display(Name = "Email adress")]
+    [Display(Name = "Email")]
     [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email adress")]
     public string Email { get; set; } = null!;
 }
